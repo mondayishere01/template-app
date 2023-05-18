8ca3e99f288d4e148d637925a1790dff399da85e
Devesh-P-081199 <devesh13579@gmail.com>
2023-05-19 01:43:42 +0530

    login page and global input style setup functions remining

--- template-app/src/app/modules/login/login.component.ts
+++ template-app/src/app/modules/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  //FormGroup
+  loginform: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.loginform = new FormGroup({
+      username: new FormControl('', [
+        Validators.pattern('^[A-Za-z][A-Za-z0-9_]{7,29}$'),
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.pattern('^[A-Za-z][A-Za-z0-9_]{7,29}$'),
+        Validators.required,
+      ]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  checkMobile(res) {
+    console.log(res);
+  }
+
+  login(res) {
+    console.log(res);
+  }
+
+  open() {
+    console.log('not yet created');
+    alert('module not created yet');
+  }
 }
--- template-app/src/app/modules/login/login.module.ts
+++ template-app/src/app/modules/login/login.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { RouterModule } from "@angular/router";
-
-
+import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
     RouterModule.forChild([
-      { path: "", component: LoginComponent, pathMatch: "full" },
+      { path: '', component: LoginComponent, pathMatch: 'full' },
     ]),
-  ]
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
