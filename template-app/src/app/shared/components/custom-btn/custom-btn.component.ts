9b0634c577be0fe5f7cd3aa43a6255f1abc435d1
mondayishere01 <devesh.pear@gmail.com>
2023-06-12 19:16:10 +0530

    adeed a component

---
+++ template-app/src/app/shared/components/custom-btn/custom-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-btn',
+  templateUrl: './custom-btn.component.html',
+  styleUrls: ['./custom-btn.component.scss']
+})
+export class CustomBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
